3c1e2855d72baa684c5b74f39e5d152c68c42410
lelin158 <lelin158@126.com>
2026-02-25 17:14:37 +0800

    Update fetchNews.js

--- api/cron/fetchNews.js
+++ api/cron/fetchNews.js
@@ -10,10 +10,22 @@ const parser = new Parser();
 
 // 您希望抓取的 RSS 源列表（可以在此添加或通过环境变量配置）
 const RSS_FEEDS = [
-  { name: 'NPR 世界新闻', url: 'https://feeds.npr.org/1001/rss.xml' },
-  { name: 'CNN 世界新闻', url: 'http://rss.cnn.com/rss/edition_world.rss' },
-  { name: '中国日报', url: 'https://www.chinadaily.com.cn/rss/world_rss.xml' },
-  // 可以继续添加更多
+  // 中文源
+  { name: '中央社 國際焦點', url: 'http://www.cna.com.tw/rss/feedForward.aspx?s=world' },
+  { name: '中国日报 世界新闻', url: 'https://www.chinadaily.com.cn/rss/world_rss.xml' },
+  { name: '工商時報 國際觀點', url: 'https://www.ctee.com.tw/rss_web/category/w-view' },
+
+  // 英文主流源
+  { name: 'BBC World News', url: 'https://feeds.bbci.co.uk/news/world/rss.xml' },
+  { name: 'CNN World News', url: 'http://rss.cnn.com/rss/edition_world.rss' },
+  { name: '路透社 World News', url: 'https://feeds.reuters.com/reuters/worldnews' },
+  { name: '卫报 World News', url: 'https://www.theguardian.com/world/rss' },
+  { name: 'NPR World News', url: 'https://feeds.npr.org/1001/rss.xml' },
+
+  // 专题/地区源
+  { name: 'MercoPress 国际', url: 'https://en.mercopress.com/rss/international' },
+  { name: 'Gulf Times 美洲', url: 'http://www.gulf-times.com/rssFeed/9/1' },
+  { name: 'Gulf Times 欧洲', url: 'http://www.gulf-times.com/rssFeed/9/2' },
 ];
 
 export default async function handler(req, res) {
